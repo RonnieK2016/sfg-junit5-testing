e4a1b9a0900ede84e968601608aa3148bafa7a84
SergeA <ronnieo@gmail.com>
2019-04-16 13:08:09 -0400

    Added Default beforeAll method called from Interface

--- src/test/java/com/example/sfgtdd/sfgjuni5testing/petclinic/ControllerTests.java
+++ src/test/java/com/example/sfgtdd/sfgjuni5testing/petclinic/ControllerTests.java
@@ -1,7 +1,16 @@
 package com.example.sfgtdd.sfgjuni5testing.petclinic;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.TestInstance;
 
 @Tag("controllers")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public interface ControllerTests {
+
+    @BeforeAll
+    default void beforeAll() {
+        System.out.println("Running beforeAll from " + ControllerTests.class.getSimpleName());
+    }
+
 }
